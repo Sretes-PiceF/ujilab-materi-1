3f8b9b3e71a0571c7ced1a98a89e1a67f2d95a05
SingaBAung <7afajarihsan@gmail.com>
2025-11-18 07:33:57 +0700

    first

---
+++ backend/app/Http/Controllers/DudiSiswa.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DudiSiswa extends Controller
+{
+    //
+}
---
+++ backend/app/Http/Controllers/MagangController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Magang;
+use App\Http\Requests\StoreMagangRequest;
+use App\Http\Requests\UpdateMagangRequest;
+
+class MagangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMagangRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Magang $magang)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Magang $magang)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMagangRequest $request, Magang $magang)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Magang $magang)
+    {
+        //
+    }
+}
